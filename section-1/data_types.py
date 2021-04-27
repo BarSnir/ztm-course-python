dfbb18f35497405bd28c27897ce15d733c9e8ebc
Bar Snir <bars@Admins-MacBook-Pro.local>
2021-04-28 00:39:06 +0300

    Developer Fundamentals 1, Section 3

---
+++ section-1/data_types.py
@@ -0,0 +1,22 @@
+# Fundamental data types
+
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes
+class superCar:
+    def __init__(self):
+        return self
+
+# Specailzed data types
+Modules = None
+None
+
+print(type(4))
+print(type(4.5))
---
+++ section-1/math_functions.py
@@ -0,0 +1,10 @@
+# Sum
+print(5+5)
+# Multiply
+print(5*5)
+# Rounded
+print(7//5)
+# Modulus
+print(7%5)
+# Round
+print(round(1.3))

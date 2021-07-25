790a287f1714fd3db73e0c2b798977e5f1efbd8f
Bar Snir <bars@Admins-MacBook-Pro.local>
2021-07-25 23:22:28 +0300

    update content

--- section-1/strings/immutability.py
+++ section-1/strings/immutability.py
@@ -1,5 +1,5 @@
 selfish = "Hello, world!"
 
-# we cannot assing selfish[0] to other string, we cant reassign
+# we cannot assing selfish[0] to other string, we can reassign
 
 selfish = selfish + " Some more content"

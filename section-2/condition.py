7844d42085b791bbfe7a654ed70396cd11cb0a4a
Bar Snir <bars@Admins-MacBook-Pro.local>
2021-07-26 00:26:56 +0300

    update content

--- section-2/condition.py
+++ section-2/condition.py
@@ -2,8 +2,13 @@ condition = True
 
 if condition and not True:
     print('It will not print.')
-elif condition or False:
+elif condition is not False:
+    print('It will print.') 
+elif condition is not 0j:
     print('It will print.') 
 else:
     print('It will not print.')
 
+
+message = "Content" if condition is True else False
+print(message)

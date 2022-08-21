a2f030b375b18a006188da9fc8c93fc52a9ab6e3
Bar Snir <bars@Admins-MacBook-Pro.local>
2022-08-21 09:42:25 +0300

    modules section

--- section-8/src/main.py
+++ section-8/src/main.py
@@ -1,4 +1,8 @@
 import utility
+from package import shopping_cart
+# OR from package.shopping_cart import add_to_shopping_cart
 
 print(utility)
 print(utility.sum(1, 2))
+cart = shopping_cart.add_to_shopping_cart({'price': 1})
+print(cart)
---
+++ section-8/src/package/shopping_cart.py
@@ -0,0 +1,7 @@
+from calendar import c
+
+
+def add_to_shopping_cart(item):
+    cart = []
+    cart.append(item)
+    return cart
